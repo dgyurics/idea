c41232b03aa3773560fde2117d37e221d6936af7
dennis <dgyurics@gmail.com>
2018-10-20 00:39:07 -0700

    implement common logging for http requests

--- src/main/java/idea/controller/AuthController.java
+++ src/main/java/idea/controller/AuthController.java
@@ -1,29 +1,23 @@
 package idea.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import idea.model.request.UserRequestModel;
 
 @RestController
+@RequestMapping("auth")
 public class AuthController {
-
-  private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-
   @PostMapping("/login")
   public void login(@RequestBody UserRequestModel user) {
-    logger.info(user.toString());
   }
 
   @PostMapping("/logout")
   public void logout(@RequestBody UserRequestModel user) {
-    logger.info(user.toString());
   }
 
   @PostMapping("/register")
   public void register(@RequestBody UserRequestModel user) {
-    logger.info(user.toString());
   }
 }
