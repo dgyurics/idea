34265a38223d1493a3aebc42df819f9f27ce9c88
dennis <dgyurics@gmail.com>
2018-11-09 00:40:23 -0800

    basic validation added UserRequestModel

--- src/main/java/idea/model/request/UserRequestModel.java
+++ src/main/java/idea/model/request/UserRequestModel.java
@@ -1,5 +1,8 @@
 package idea.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,8 +11,13 @@ import lombok.ToString;
 @Getter @Setter
 public class UserRequestModel {
   private Long id;
+  @NotEmpty
+  @Size(min = 3,max = 30)
   private String username;
+  @Email
   private String email;
+  @NotEmpty
+  @Size(min = 5,max = 100)
   @ToString.Exclude
   private String password;
 }
